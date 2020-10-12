fd5372fc586ba720e419867b5c5dd3893d31aa46
Olivier Blaivie <o.blaivie@coperbee.fr>
2020-10-12 13:40:21 +0200

    Creation des icones

---
+++ appFormation/src/app/icons/icon-edit/icon-edit.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-icon-edit',
+  templateUrl: './icon-edit.component.html',
+  styleUrls: ['./icon-edit.component.scss']
+})
+export class IconEditComponent implements OnInit {
+
+  public iconedit = faEdit;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- appFormation/src/app/icons/icons.module.ts
+++ appFormation/src/app/icons/icons.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconNavComponent } from './icon-nav/icon-nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconEditComponent } from './icon-edit/icon-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [IconNavComponent, IconEditComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
+  ], exports: [
+    IconNavComponent,
+    IconEditComponent
   ]
 })
 export class IconsModule { }
